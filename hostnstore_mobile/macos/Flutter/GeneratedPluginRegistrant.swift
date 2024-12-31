b4079a7136343746a76d99a7abc755636d8d36f0
supunnilakshana <supunnilakshana1999@gmail.com>
2024-12-31 11:25:56 +0530

    WIP

--- hostnstore_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hostnstore_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import apple_vision_selfie
+import file_selector_macos
+import flutter_secure_storage_macos
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppleVisionSelfiePlugin.register(with: registry.registrar(forPlugin: "AppleVisionSelfiePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
